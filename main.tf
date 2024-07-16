4bb80bc268a21fc34780c51d89fee6d8b22b19b6
miguel-hdez-hashicorp <miguel.hernandez1@hashicorp.com>
2024-07-16 21:16:42 +0200

    removing ssh

--- main.tf
+++ main.tf
@@ -39,12 +39,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   network_interface_ids = [
     azurerm_network_interface.nic.id,
   ]
-
-  admin_ssh_key {
-    username   = var.admin_username
-    public_key = file("~/.ssh/id_rsa.pub")
-  }
-
+  
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
